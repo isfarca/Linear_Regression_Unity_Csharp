436aa742dd54f2e54eb293df7a648138d330c4da
Fethi Isfarca <fisfarca@googlemail.com>
2019-02-07 22:30:57 +0100

    Adding project.

---
+++ Linear Regression/Linear Regression/Main.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Linear_Regression
+{
+    partial class FormLinearRegression
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.LabelDataPoints = new System.Windows.Forms.Label();
+            this.LabelDegree = new System.Windows.Forms.Label();
+            this.ButtonPlotData = new System.Windows.Forms.Button();
+            this.ButtonPlotLine = new System.Windows.Forms.Button();
+            this.ChartLinearRegression = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.NumericUpDownDataPoints = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDownDegree = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartLinearRegression)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownDataPoints)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownDegree)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LabelDataPoints
+            // 
+            this.LabelDataPoints.AutoSize = true;
+            this.LabelDataPoints.Location = new System.Drawing.Point(75, 37);
+            this.LabelDataPoints.Name = "LabelDataPoints";
+            this.LabelDataPoints.Size = new System.Drawing.Size(96, 20);
+            this.LabelDataPoints.TabIndex = 0;
+            this.LabelDataPoints.Text = "Data Points:";
+            // 
+            // LabelDegree
+            // 
+            this.LabelDegree.AutoSize = true;
+            this.LabelDegree.Location = new System.Drawing.Point(105, 79);
+            this.LabelDegree.Name = "LabelDegree";
+            this.LabelDegree.Size = new System.Drawing.Size(66, 20);
+            this.LabelDegree.TabIndex = 1;
+            this.LabelDegree.Text = "Degree:";
+            // 
+            // ButtonPlotData
+            // 
+            this.ButtonPlotData.Location = new System.Drawing.Point(423, 37);
+            this.ButtonPlotData.Name = "ButtonPlotData";
+            this.ButtonPlotData.Size = new System.Drawing.Size(85, 27);
+            this.ButtonPlotData.TabIndex = 4;
+            this.ButtonPlotData.Text = "Plot Data";
+            this.ButtonPlotData.UseVisualStyleBackColor = true;
+            this.ButtonPlotData.Click += new System.EventHandler(this.ButtonPlotData_Click);
+            // 
+            // ButtonPlotLine
+            // 
+            this.ButtonPlotLine.Location = new System.Drawing.Point(423, 76);
+            this.ButtonPlotLine.Name = "ButtonPlotLine";
+            this.ButtonPlotLine.Size = new System.Drawing.Size(85, 27);
+            this.ButtonPlotLine.TabIndex = 5;
+            this.ButtonPlotLine.Text = "Plot Line";
+            this.ButtonPlotLine.UseVisualStyleBackColor = true;
+            this.ButtonPlotLine.Click += new System.EventHandler(this.ButtonPlotLine_Click);
+            // 
+            // ChartLinearRegression
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.ChartLinearRegression.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.ChartLinearRegression.Legends.Add(legend3);
+            this.ChartLinearRegression.Location = new System.Drawing.Point(12, 141);
+            this.ChartLinearRegression.Name = "ChartLinearRegression";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.ChartLinearRegression.Series.Add(series3);
+            this.ChartLinearRegression.Size = new System.Drawing.Size(586, 444);
+            this.ChartLinearRegression.TabIndex = 6;
+            this.ChartLinearRegression.Text = "Linear Regression";
+            // 
+            // NumericUpDownDataPoints
+            // 
+            this.NumericUpDownDataPoints.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.NumericUpDownDataPoints.Location = new System.Drawing.Point(231, 35);
+            this.NumericUpDownDataPoints.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.NumericUpDownDataPoints.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.NumericUpDownDataPoints.Name = "NumericUpDownDataPoints";
+            this.NumericUpDownDataPoints.Size = new System.Drawing.Size(120, 26);
+            this.NumericUpDownDataPoints.TabIndex = 7;
+            this.NumericUpDownDataPoints.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // NumericUpDownDegree
+            // 
+            this.NumericUpDownDegree.Location = new System.Drawing.Point(231, 77);
+            this.NumericUpDownDegree.Name = "NumericUpDownDegree";
+            this.NumericUpDownDegree.Size = new System.Drawing.Size(120, 26);
+            this.NumericUpDownDegree.TabIndex = 8;
+            // 
+            // FormLinearRegression
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 597);
+            this.Controls.Add(this.NumericUpDownDegree);
+            this.Controls.Add(this.NumericUpDownDataPoints);
+            this.Controls.Add(this.ChartLinearRegression);
+            this.Controls.Add(this.ButtonPlotLine);
+            this.Controls.Add(this.ButtonPlotData);
+            this.Controls.Add(this.LabelDegree);
+            this.Controls.Add(this.LabelDataPoints);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "FormLinearRegression";
+            this.ShowIcon = false;
+            this.Text = "Linear Regression";
+            ((System.ComponentModel.ISupportInitialize)(this.ChartLinearRegression)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownDataPoints)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDownDegree)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelDataPoints;
+        private System.Windows.Forms.Label LabelDegree;
+        private System.Windows.Forms.Button ButtonPlotData;
+        private System.Windows.Forms.Button ButtonPlotLine;
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartLinearRegression;
+        private System.Windows.Forms.NumericUpDown NumericUpDownDataPoints;
+        private System.Windows.Forms.NumericUpDown NumericUpDownDegree;
+    }
+}
+
---
+++ Linear Regression/Linear Regression/Main.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Linear_Regression
+{
+    public partial class FormLinearRegression : Form
+    {
+        // Declare variables.
+        private Random random = new Random();
+        private double[] xCoordinates = new double[] { };
+        private double[] yCoordinates = new double[] { };
+
+        /// <summary>
+        /// Start the form.
+        /// </summary>
+        public FormLinearRegression()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Write coordinate data in diagram.
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">Event.</param>
+        private void ButtonPlotData_Click(object sender, EventArgs e)
+        {
+            // Clear the chart.
+            ChartLinearRegression.Titles.Clear();
+            ChartLinearRegression.Series.Clear();
+
+            // Get the amount of the coordinates.
+            xCoordinates = new double[Convert.ToInt32(NumericUpDownDataPoints.Value)];
+            yCoordinates = new double[Convert.ToInt32(NumericUpDownDataPoints.Value)];
+
+            // Title of the chart.
+            ChartLinearRegression.Titles.Add("Linear Regression");
+
+            // Get the coordinates.
+            for (int i = 0; i < Math.Floor(xCoordinates.GetLength(0) / 2.0d); i++)
+                xCoordinates[i] = random.Next(i - xCoordinates.Length / 10, i + xCoordinates.Length / 10);
+
+            for (int i = Convert.ToInt32(Math.Floor(xCoordinates.GetLength(0) / 2.0d)); i < xCoordinates.GetLength(0); i++)
+                xCoordinates[i] = random.Next(i + xCoordinates.Length / 5, i + xCoordinates.Length / 2);
+
+            for (int i = 0; i < Math.Floor(yCoordinates.GetLength(0) / 2.0d); i++)
+                yCoordinates[i] = random.Next(i, i + yCoordinates.Length / 5);
+
+            for (int i = Convert.ToInt32(Math.Floor(yCoordinates.GetLength(0) / 2.0d)); i < yCoordinates.GetLength(0); i++)
+                yCoordinates[i] = random.Next(i - yCoordinates.Length / 10, i);
+
+            // Set the settings of the coordinates and add the points in diagram. 
+            ChartLinearRegression.Series.Add("Data Points");
+            ChartLinearRegression.Series["Data Points"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+            for (int i = 0; i < xCoordinates.Length; i++)
+                ChartLinearRegression.Series["Data Points"].Points.AddXY(xCoordinates[i], yCoordinates[i]);
+
+            ChartLinearRegression.Series["Data Points"].Color = Color.DarkOrange;
+
+            ChartLinearRegression.Series.Add("QR Line");
+            ChartLinearRegression.Series["QR Line"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            ChartLinearRegression.Series["QR Line"].Color = Color.DarkGreen;
+        }
+
+        /// <summary>
+        /// Write the gradient triangle for the graph in diagram (machine learning).
+        /// </summary>
+        /// <param name="sender">Object.</param>
+        /// <param name="e">Event.</param>
+        private void ButtonPlotLine_Click(object sender, EventArgs e)
+        {
+            // Declare variables.
+            int degree = Convert.ToInt32(NumericUpDownDegree.Value);
+            double xMin, xMax, interval;
+            double[] qrTheta;
+            DenseMatrix x = new DenseMatrix(xCoordinates.Length, degree + 1);
+            DenseMatrix y;
+
+            // Clear the points.
+            ChartLinearRegression.Series["QR Line"].Points.Clear();
+
+            x.SetColumn(0, DenseVector.Create(xCoordinates.Length, i => 1));
+
+            if (degree != 0)
+                x.SetColumn(1, xCoordinates);
+
+            for (int i = 2; i <= degree; i++)
+                x.SetColumn(i, x.Column(1).PointwiseMultiply(x.Column(i - 1)));
+
+            y = DenseMatrix.OfColumns(yCoordinates.Length, 1, new[] { new DenseVector(yCoordinates) });
+
+            qrTheta = x.QR().Solve(y).ToColumnMajorArray();
+
+            xMin = xCoordinates.Min();
+            xMax = xCoordinates.Max();
+            interval = (xMax - xMin) / Convert.ToDouble(NumericUpDownDegree.Value);
+
+            // Set the points.
+            for (int i = 0; i < xCoordinates.Length; i++)
+                ChartLinearRegression.Series["QR Line"].Points.AddXY(xCoordinates[i], Yprediction(xCoordinates[i], qrTheta));
+        }
+
+        /// <summary>
+        /// Determine the predition.
+        /// </summary>
+        /// <param name="xPlot">X plot of the diagram.</param>
+        /// <param name="theta">Value for statistic.</param>
+        /// <returns></returns>
+        private static double Yprediction(double xPlot, double[] theta)
+        {
+            // Declare variables.
+            double yPlot = 0.0d;
+
+            for (int i = 0; i < theta.Length; i++)
+                yPlot += theta[i] * Math.Pow(xPlot, i);
+
+            return yPlot;
+        }
+    }
+}
